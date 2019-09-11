38af58e1a12c8ddc764e7669a937af7a43a91784
yrion <yuldong@126.com>
2019-09-11 09:13:58 +0800

    feature:
    第6天demo

---
+++ rumen/day62.js
@@ -0,0 +1,10 @@
+let click2Scroll = function() {
+    let dBox = document.getElementById('d');
+    if (dBox) {
+        let left = dBox.offsetLeft;
+        let navBox = document.getElementById('nav-box');
+        if (navBox) {
+            navBox.scrollTo(navBox.clientWidth / 2 - dBox.clientWidth - 15, 0);
+        }
+    }
+}
